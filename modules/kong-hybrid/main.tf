0a433b0c60b0601c2429e5f58ce8daad6f7380e1
tunghoang <tung.hoang1@onemount.com>
2020-10-15 18:39:55 +0700

    add timeout loyalty

--- modules/kong-hybrid/main.tf
+++ modules/kong-hybrid/main.tf
@@ -123,7 +123,7 @@ resource "google_compute_instance" "kong-admin" {
 
     network_interface {
         network = var.network
-        subnetwork = var.sub_networks[1]
+        subnetwork = var.sub_networks[0]
         access_config {
             // Ephemeral IP
         }
